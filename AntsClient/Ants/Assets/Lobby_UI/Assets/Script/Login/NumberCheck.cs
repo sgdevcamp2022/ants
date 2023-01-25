bb8e07e84fda73661cca2d6848e9199059df2688
minji2219 <119796600+minji2219@users.noreply.github.com>
2023-01-25 16:27:54 +0900

    클라이언트 작업공간 분할

---
+++ AntsClient/Ants/Assets/Lobby_UI/Assets/Script/Login/NumberCheck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+using TMPro;
+//using System.Runtime.InteropServices;
+
+public class NumberCheck : MonoBehaviour
+{
+
+    //public GameObject Login;
+    public InputField number;
+    public GameObject text;
+    public TextMeshProUGUI textMeshPro;
+
+    string fakeNum = "1234";
+    public void OnClickCheck()
+    {
+        
+        if (fakeNum == number.text)
+        {
+            text.SetActive(true);
+        }
+        else
+        {
+            textMeshPro.text = "different";
+            text.SetActive(true);
+            //number.text = "";
+            //number.ActivateInputField();
+            //OnClickCheck();>>오류 어떻게 OnClcik함수로 갈 수 있지?
+            //받은 값으로 OnclickCheck()함수 실행 되어야함
+        }
+
+
+
+    }
+}
+
+
---
+++ AntsClient/Ants/Assets/Lobby_UI/Assets/Script/Login/pwConfirm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Xml.Serialization;
+
+public class pwConfirm : MonoBehaviour
+{
+    //pw에 입력된 값과 confirm에 입력된 값이 같다면 same 글씨 뜨게하기
+    public TMP_InputField pw;
+    public TMP_InputField PwConfirm;
+    public GameObject text;
+    public TextMeshProUGUI textMeshPro;
+
+   public void PwSame()
+    {
+        if(pw.text == PwConfirm.text)
+        {
+            text.SetActive(true);
+        }
+        else
+        {
+            textMeshPro.SetText("different");
+            text.SetActive(true);
+        }
+    }
+
+}
