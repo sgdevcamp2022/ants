dca7dbfa2a230d192d284d762a65f540b2f8cb88
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2023-01-26 13:53:21 +0900

    외부 에셋 설정 완료
    유니티 에셋스토어 외부 에셋 설정완료.

---
+++ AntsClient/Ants/Assets/InGame/Script/DB/DatabaseManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+
+    public static int stat;
+
+
+    static public DatabaseManager instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+
+    }
+    }
