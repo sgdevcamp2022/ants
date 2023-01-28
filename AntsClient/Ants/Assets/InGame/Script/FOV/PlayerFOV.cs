4648db1ae0c4ca193af0d3976cab632f97c92c22
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2023-01-28 15:12:08 +0900

    FOV 구현 완료
    시야 시스템 구현 완료. 단 아직 테두리에 흰 선이 보이는등 그래픽에서 수정해줄 부분 필요. 또한 아직 주석 처리등 협업을 위한 구조는 다듬어야 할 필요성이 있음.

---
+++ AntsClient/Ants/Assets/InGame/Script/FOV/PlayerFOV.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFOV : MonoBehaviour
+{
+
+    float angle;
+    Vector2 target, mouse;
+
+    [SerializeField] private FieldOfView fieldOfView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        */
+
+
+        Vector3 targetposition = GetMouseWorldPosition();
+        Vector3 aimDir = (targetposition - transform.position).normalized;
+
+
+        fieldOfView.SetAimDir(aimDir);
+        fieldOfView.setOrigin(transform.position);
+
+
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
