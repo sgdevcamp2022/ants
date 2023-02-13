43ecc946970bd0ffe667bb70a8e8bcb2eb3f6ec0
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2023-02-14 01:07:28 +0900

    [기능 수정] 충돌되는 파일 수정

--- .github/pull_request_template.md
+++
@@ -1,5 +0,0 @@
-## 작업 내용
-
-## 스크린샷 (Optional)
-
-## To Reviewers
--- gameServer/gameServer/Constant.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-const int MAX_SESSION_COUNT = 100;
-
-const unsigned short PORT_NUMBER = 10006;
-
-const int MAX_RECEIVE_BUFFER_LEN = 512;
