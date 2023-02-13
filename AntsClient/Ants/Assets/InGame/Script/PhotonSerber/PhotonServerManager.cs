d7df065b2f5b4c0e5d4a434a327b7a487b8dad3b
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2023-02-13 17:20:51 +0900

    [기능 추가] 포톤 서버 에셋 추가, 연동 진행중

---
+++ AntsClient/Ants/Assets/InGame/Script/PhotonSerber/PhotonServerManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+public class PhotonServerManager : MonoBehaviourPunCallbacks
+{
+    public InputField NickNameInput;
+    public GameObject DisconnectPanel;
+    public GameObject respawnPanel;
+}
