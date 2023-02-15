6d736a461c84bf941885b530fe1c624676e7ce9b
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2023-02-15 15:59:41 +0900

    [기능 추가] PVP 서버 연동 진행중

---
+++ AntsClient/Ants/Assets/InGame/Script/Packet/PacketHandler.cs
@@ -0,0 +1,23 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.Protocol;
+using ServerCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PacketHandler
+{
+	public static void M_TestHandler(PacketSession session, IMessage packet)
+	{
+		M_Test chatPacket = packet as M_Test;
+		ServerSession serverSession = session as ServerSession;
+
+		Debug.Log(chatPacket.Msg);
+	}
+
+	public static void M_InitroomHandler(PacketSession session, IMessage packet)
+	{
+		M_Initroom enterGamePacket = packet as M_Initroom;
+		ServerSession serverSession = session as ServerSession;
+	}
+}
